9166a0dd0f9bd45a67b6f55e5b3b9b3e425e5258
Jessica Wiersma <jesswiersma@gmail.com>
2021-04-22 15:41:57 -0400

    a few practice routes

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const Express =require('express');
+const { ecommerceController } = require('./controllers');
 const app = Express();
 
+const controllers = require("./controllers");
+
+////TEST ROUTES FOR SERVER SET UP///
+
+app.use('/test', (req, res) => {
+    res.send("This is a message from the test endpoint on the server!")
+});
+
+//////////////////////////////////////////////////
+
+app.use('/ecommerce', controllers.ecommerceController);
+
+app.use('/about', controllers.ecommerceController);
+
+
+
 app.listen(3000, () => {
-    console.log('[Server]: App is listening on 3000.');
+    console.log(`[Server]: App is listening on 3000.`);
 });
