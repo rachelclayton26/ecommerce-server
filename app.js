3974623f6a58c4806f418a1311965c954dc42981
Rachel Christopher <rachelchristopher@Rachels-MBP.wsfdindl.metronetinc.net>
2021-04-23 16:42:46 -0400

    4:42

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const Express =require('express');
+const Express = require('express');
 const app = Express();
+
+// app.use('/test', (re,res) => {
+//     res.send('This is a message from the test endpoint on the server!')
+// })
 
 app.listen(3000, () => {
     console.log('[Server]: App is listening on 3000.');
